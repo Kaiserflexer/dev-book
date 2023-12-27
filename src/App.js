5a9363db24d7705d20e3efb249da75f7e2bd4f40
Alex <desert1thunder@gmail.com>
2023-12-27 16:00:26 +0200

    Replace files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import { Sandpack, SandpackProvider, SandpackReactContext, SandpackLayout, SandpackStack, CodeEditor, SandpackPreview } from "@codesandbox/sandpack-react";
+import { monokaiPro } from "@codesandbox/sandpack-themes";
+const App = () => {
+  const files = {}
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     
+    
+    <SandpackProvider
+      files={files} 
+      template="react"
+      theme={monokaiPro} 
+      
+    >
+      <SandpackReactContext>
+        {({ files, updateFile }) => {
+          const fileListValues = Object.values(files);
+          const fileListKeys = Object.keys(files);
+
+          return (
+            <SandpackLayout>
+              <SandpackStack style={{ padding: "10px 0", height: "900px" }}>
+                <CodeEditor
+                  code={fileListValues[0].code}
+                  filePath={fileListKeys[0]}
+                  onCodeUpdate={(newCode) =>
+                    updateFile(fileListKeys[0], newCode)
+                  }
+                  showTabs
+                />
+              </SandpackStack>
+
+              <SandpackStack style={{ padding: "10px 0", height: "900px"}}>
+                <CodeEditor
+                  code={fileListValues[1].code}
+                  filePath={fileListKeys[1]}
+                  onCodeUpdate={(newCode) =>
+                    updateFile(fileListKeys[1], newCode)
+                  }
+                  showTabs
+                  
+                />
+              </SandpackStack>
+    
+              <SandpackPreview style={{ height: "900px"}} />
+            </SandpackLayout>
+          );
+        }}
+      </SandpackReactContext>
+    </SandpackProvider>
+    
+  )  
 }
 
 export default App;
